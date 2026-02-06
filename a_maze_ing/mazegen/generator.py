8f1db9719d261c5fe0c24b04de777d60b70bb31c
Pownek-code <lizzotuco0@gmail.com>
2026-02-06 13:23:18 -0800

    added make_route function.

--- a_maze_ing/mazegen/generator.py
+++ a_maze_ing/mazegen/generator.py
@@ -1,7 +1,27 @@
+import random
 wall = "#"
 path = " "
-def init_grid(width: int, height: int):
+def make_route(grid: list, x: int, y: int):
+    grid[y][x] = path
+    print(grid)
+
+
+def init_grid(width: int, height: int) -> list:
     grid = []
     for i in range(height):
         grid.append([wall] * width)
+    # if check_valid(width, height, grid) == True:
+    #     result = make_route(grid, width, height)
+    # else:
+    #     result = grid.clear()
+    print(len(grid[0]))
     return grid
+
+def check_valid(x: int, y: int, grid: list) -> bool:
+    if x < 0 or x >= len(grid[0]) or y < 0 or y >= len(grid):
+        return False
+    if grid[y][x] != wall:
+        return False
+    return True
+
+print(check_valid(5, 4, init_grid(4, 5)))
