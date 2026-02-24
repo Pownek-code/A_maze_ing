f486f8a79ab3f05dbf17ce965f1fabe40598854c
Pownek-code <lizzotuco0@gmail.com>
2026-02-24 07:03:08 -0800

    setup and building the project, plus a venv to resolve internal issues.

---
+++ a_maze_ing/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="mazegen",
+    version="1.0.0",
+    packages=find_packages(),
+    description="A Python module for algorithmic maze generation and graph traversal.",
+    author="schanqou",
+    python_requires=">=3.10",
+)
