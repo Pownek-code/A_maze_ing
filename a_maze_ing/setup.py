fc60bf8ed4cd1cb880335ef96695e98961a978b4
Pownek-code <lizzotuco0@gmail.com>
2026-02-24 07:43:46 -0800

    updated makefile and setup.

--- a_maze_ing/setup.py
+++ a_maze_ing/setup.py
@@ -1,9 +1,9 @@
-from setuptools import setup, find_packages
+from setuptools import setup
 
 setup(
     name="mazegen",
     version="1.0.0",
-    packages=find_packages(),
+    packages=["mazegen"],
     description="A Python module for algorithmic maze generation and graph traversal.",
     author="schanqou",
     python_requires=">=3.10",
